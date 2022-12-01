61052373cd14c1c27da0b4547bea81dbb4649c4f
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2022-12-01 21:20:42 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+  var start=0;
+  var end = str.length()-1;
+  while(start<end){
+    if(str.charAt(start) != str.charAt(end)) return false;
+  }
+  return true;
 }
 module.exports = palindrome
